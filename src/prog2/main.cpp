fcef8a1ca06a27bbc3297f36118050ae464b62cc
ram-gudapati <ramarao.gudapati@gmail.com>
2024-12-05 14:41:21 -0500

    Update main.cpp

--- src/prog2/main.cpp
+++ src/prog2/main.cpp
@@ -4,7 +4,6 @@
 #include "../lib2/lib2.hpp"
 
 int main(int argc, char *argv[]) {
-    banner();
 
     printf("libfunc1(%d) call: %d\n", 2, libfunc1(2));
     printf("libfunc2(%d) call: %d\n", 2, libfunc2(2));
